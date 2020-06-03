a3a4e0c0e4287b8e383f6fd57865c7e1fe588f10
jbrussell <jbrussell93@gmail.com>
2020-06-03 15:14:53 -0400

    update binpath

--- set_binpath.m
+++ set_binpath.m
@@ -1,6 +1,6 @@
 function [] = set_binpath()
     % Path to mineos fortran binaries
-    path2BIN = '../../MINEOS/bin';
+    path2BIN = '../MINEOS/bin';
     % Path to gtimeout
     path2gtimeout = '/usr/local/bin';
     setenv('PATH', [path2BIN,':',path2gtimeout,':',getenv('PATH')]);
